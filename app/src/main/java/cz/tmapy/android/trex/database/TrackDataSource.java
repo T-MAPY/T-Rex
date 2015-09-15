2c25be05898791904a118a38647f2b70c824b04b
Kamil Svoboda <kamil.svoboda@tmapy.cz>
2015-09-15 17:01:56 +0200

    new dob for storing tracks

---
+++ app/src/main/java/cz/tmapy/android/trex/database/TrackDataSource.java
@@ -0,0 +1,178 @@
+package cz.tmapy.android.trex.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.renderscript.ScriptIntrinsicYuvToRGB;
+import android.util.Log;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.tmapy.android.trex.database.dobs.TrackDob;
+
+/**
+ * Created by kasvo on 15.9.2015.
+ */
+public class TrackDataSource {
+    private static final String TAG = LocationsDataSource.class.getName();
+
+    private static final String TABLE_NAME = "tracks";
+    private static final String COL_ID = "_id"; //The database tables should use the identifier _id for the primary key of the table. Several Android functions rely on this standard.
+    private static final String START_TIME = "start_time";
+    private static final String START_LAT = "start_lat";
+    private static final String START_LON = "start_lon";
+    private static final String START_ADDRESS = "start_address";
+    private static final String FINISH_TIME = "finish_time";
+    private static final String FINISH_LAT = "finish_lat";
+    private static final String FINISH_LON = "finish_lon";
+    private static final String FINISH_ADDRESS = "finish_address";
+    private static final String MAX_SPEED = "max_speed";
+    private static final String AVE_SPEED = "ave_speed";
+    private static final String MIN_ALT = "min_alt";
+    private static final String MAX_ALT = "max_alt";
+    private static final String ELEV_DIFF_UP = "elev_diff_up";
+    private static final String ELEV_DIFF_DOWN = "elev_diff_down";
+    private static final String NOTE = "note";
+    private static final String COL_UPDATE_TIME = "update_time";
+    private static final String IDX_ID = "id_idx";
+
+    public static final String DROP_TABLE_SQL = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+
+    public static final String CREATE_TABLE_SQL = "CREATE TABLE " + TABLE_NAME + " (" +
+            COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+            START_TIME + " INTEGER, " +
+            START_LAT + " REAL, " +
+            START_LON + " REAL, " +
+            START_ADDRESS + " TEXT, " +
+            FINISH_TIME + " INTEGER, " +
+            FINISH_LAT + " REAL, " +
+            FINISH_LON + " REAL, " +
+            FINISH_ADDRESS + " TEXT," +
+            MAX_SPEED + " REAL, " +
+            AVE_SPEED + " REAL, " +
+            MIN_ALT + " REAL, " +
+            MAX_ALT + " REAL, " +
+            ELEV_DIFF_UP + " REAL, " +
+            ELEV_DIFF_DOWN + " REAL, " +
+            NOTE + " TEXT, " +
+            COL_UPDATE_TIME + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP);";
+
+    public static final String CREATE_INDEX = "CREATE INDEX " + IDX_ID + " ON " + TABLE_NAME + " (" + COL_ID + ");";
+
+    // Database fields
+    private SQLiteDatabase database;
+    private DatabaseHelper dbHelper;
+    private String[] allColumns = {COL_ID, START_TIME, START_LAT, START_LON, START_ADDRESS, FINISH_TIME, FINISH_LAT, FINISH_LON, FINISH_ADDRESS, MAX_SPEED, AVE_SPEED, MIN_ALT, MAX_ALT, ELEV_DIFF_UP, ELEV_DIFF_DOWN, NOTE, COL_UPDATE_TIME};
+
+    /**
+     * Static method to create table
+     *
+     * @param db
+     */
+    public static void InitTable(SQLiteDatabase db) {
+        db.execSQL(DROP_TABLE_SQL);
+        db.execSQL(CREATE_TABLE_SQL);
+        db.execSQL(CREATE_INDEX);
+    }
+
+    public TrackDataSource(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    /**
+     * Complete table erase
+     *
+     * @throws SQLException
+     */
+    public void EraseTable() {
+        try {
+            open();
+            InitTable(database); //sqlite doesn't have TRUNCATE - drop is recomended
+            close();
+        } catch (Exception e) {
+            Log.e(TAG, "Cannot erase table", e);
+        }
+    }
+
+    public Long saveTrack(TrackDob trackDob) {
+        long insertId = -1;
+        try {
+            open();
+            ContentValues values = new ContentValues();
+            values.put(START_TIME, trackDob.getStartTime());
+            values.put(START_LAT, trackDob.getStartLat());
+            values.put(START_LON, trackDob.getStartLon());
+            values.put(START_ADDRESS, trackDob.getStartAddress());
+            values.put(FINISH_TIME, trackDob.getFinishTime());
+            values.put(FINISH_LAT, trackDob.getFinishLat());
+            values.put(FINISH_LON, trackDob.getFinishLon());
+            values.put(FINISH_ADDRESS, trackDob.getFinishAddress());
+            values.put(MAX_SPEED, trackDob.getMaxSpeed());
+            values.put(AVE_SPEED, trackDob.getAveSpeed());
+            values.put(MIN_ALT, trackDob.getMinAlt());
+            values.put(MAX_ALT, trackDob.getMaxAlt());
+            values.put(ELEV_DIFF_UP, trackDob.getElevDiffUp());
+            values.put(ELEV_DIFF_DOWN, trackDob.getElevDiffDown());
+            values.put(NOTE, trackDob.getNote());
+            insertId = database.insert(TABLE_NAME, null, values);
+            close();
+        } catch (Exception e) {
+            Log.e(TAG, "Cannot save track", e);
+        }
+
+        return insertId;
+    }
+
+    public List<TrackDob> getAllLocations() {
+        List<TrackDob> locations = new ArrayList<TrackDob>();
+        try {
+            open();
+            Cursor cursor = database.query(TABLE_NAME, allColumns, null, null, null, null, null);
+
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                TrackDob loc = cursorToLocation(cursor);
+                locations.add(loc);
+                cursor.moveToNext();
+            }
+            // make sure to close the cursor
+            cursor.close();
+            close();
+        } catch (Exception e) {
+            Log.e(TAG, "Cannot read all tracks", e);
+        }
+        return locations;
+    }
+
+    private TrackDob cursorToLocation(Cursor cursor) {
+        TrackDob trackDob = new TrackDob();
+        trackDob.setId(cursor.getLong(0));
+        trackDob.setStartTime(cursor.getLong(1));
+        trackDob.setStartLat(cursor.getDouble(2));
+        trackDob.setStartLon(cursor.getDouble(3));
+        trackDob.setStartAddress(cursor.getString(4));
+        trackDob.setFinishTime(cursor.getLong(5));
+        trackDob.setFinishLat(cursor.getDouble(6));
+        trackDob.setFinishLon(cursor.getDouble(7));
+        trackDob.setMaxSpeed(cursor.getFloat(8));
+        trackDob.setAveSpeed(cursor.getFloat(9));
+        trackDob.setMinAlt(cursor.getDouble(10));
+        trackDob.setMaxAlt(cursor.getDouble(11));
+        trackDob.setElevDiffUp(cursor.getFloat(12));
+        trackDob.setElevDiffDown(cursor.getFloat(13));
+        trackDob.setNote(cursor.getString(14));
+        trackDob.setUpdateTime(cursor.getLong(15));
+        return trackDob;
+    }
+}
---
+++ app/src/main/java/cz/tmapy/android/trex/database/dobs/TrackDob.java
@@ -0,0 +1,180 @@
+package cz.tmapy.android.trex.database.dobs;
+
+/**
+ * Created by kasvo on 15.9.2015.
+ */
+public class TrackDob {
+    private Long id;
+    private Long startTime;
+    private Double startLat;
+    private Double startLon;
+    private String startAddress;
+    private Long finishTime;
+    private Double finishLat;
+    private Double finishLon;
+    private String finishAddress;
+    private Float maxSpeed;
+    private Float aveSpeed;
+    private Double minAlt;
+    private Double maxAlt;
+    private Float elevDiffUp;
+    private Float elevDiffDown;
+    private String note;
+    private Long updateTime;
+
+    public TrackDob(){};
+
+    public TrackDob(Long startTime, Double startLat, Double startLon, String startAddress, Long finishTime, Double finishLat, Double finishLon, String finishAddress, Float maxSpeed, Float aveSpeed, Double minAlt, Double maxAlt, Float elevDiffUp, Float elevDiffDown, String note) {
+        this.startTime = startTime;
+        this.startLat = startLat;
+        this.startLon = startLon;
+        this.startAddress = startAddress;
+        this.finishTime = finishTime;
+        this.finishLat = finishLat;
+        this.finishLon = finishLon;
+        this.finishAddress = finishAddress;
+        this.maxSpeed = maxSpeed;
+        this.aveSpeed = aveSpeed;
+        this.minAlt = minAlt;
+        this.maxAlt = maxAlt;
+        this.elevDiffUp = elevDiffUp;
+        this.note = note;
+        this.elevDiffDown = elevDiffDown;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Long startTime) {
+        this.startTime = startTime;
+    }
+
+    public Double getStartLat() {
+        return startLat;
+    }
+
+    public void setStartLat(Double startLat) {
+        this.startLat = startLat;
+    }
+
+    public Double getStartLon() {
+        return startLon;
+    }
+
+    public void setStartLon(Double startLon) {
+        this.startLon = startLon;
+    }
+
+    public String getStartAddress() {
+        return startAddress;
+    }
+
+    public void setStartAddress(String startAddress) {
+        this.startAddress = startAddress;
+    }
+
+    public Long getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(Long finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public Double getFinishLat() {
+        return finishLat;
+    }
+
+    public void setFinishLat(Double finishLat) {
+        this.finishLat = finishLat;
+    }
+
+    public Double getFinishLon() {
+        return finishLon;
+    }
+
+    public void setFinishLon(Double finishLon) {
+        this.finishLon = finishLon;
+    }
+
+    public String getFinishAddress() {
+        return finishAddress;
+    }
+
+    public void setFinishAddress(String finishAddress) {
+        this.finishAddress = finishAddress;
+    }
+
+    public Float getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(Float maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Float getAveSpeed() {
+        return aveSpeed;
+    }
+
+    public void setAveSpeed(Float aveSpeed) {
+        this.aveSpeed = aveSpeed;
+    }
+
+    public Double getMinAlt() {
+        return minAlt;
+    }
+
+    public void setMinAlt(Double minAlt) {
+        this.minAlt = minAlt;
+    }
+
+    public Double getMaxAlt() {
+        return maxAlt;
+    }
+
+    public void setMaxAlt(Double maxAlt) {
+        this.maxAlt = maxAlt;
+    }
+
+    public Float getElevDiffUp() {
+        return elevDiffUp;
+    }
+
+    public void setElevDiffUp(Float elevDiffUp) {
+        this.elevDiffUp = elevDiffUp;
+    }
+
+    public Float getElevDiffDown() {
+        return elevDiffDown;
+    }
+
+    public void setElevDiffDown(Float elevDiffDown) {
+        this.elevDiffDown = elevDiffDown;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Long getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Long updateTime) {
+        this.updateTime = updateTime;
+    }
+}
